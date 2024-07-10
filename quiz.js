0b91415761a3b9c60bf8ea41cdc7aff294af1f78
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-07-10 20:57:30 +0300

    Implement a Simple Interactive Quiz

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer(){
+    const correctAnswer = 4;
+    const userAnswer = document.querySelector('input[type="radio"][name="quiz"]:checked').value;
+   console.log(userAnswer);
+        if (correctAnswer == userAnswer){
+            document.getElementById('feedback').textContent =  "Correct! Well done."
+        }
+        else{
+            document.getElementById('feedback').textContent =  "That's incorrect. Try again!"
+        }  
+
+}
+document.getElementById('submit-answer').addEventListener('click', checkAnswer)
